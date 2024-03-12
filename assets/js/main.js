30cb4d2379a23c2ad6823fda2429f50cfb5b06fc
Vadim Khoroshev <vs.khoroshev@gmail.com>
2024-03-12 18:39:55 +0300

    commit-240312

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,18 +3,19 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 function init() {
-    console.log("Hello!!!!")
 
-    document.querySelector(":root").classList.toggle("light");
     document.querySelector(".light-theme-icon").classList.toggle("hidden");
-    // document.querySelector(":root").classList.toggle("dark");
+    document.querySelector(":root").classList.toggle("light");
 
-    // document.querySelector(".language-switcher-button").addEventListener("click", function () {
-    //     document.querySelector(".language-switcher-list").classList.toggle("hidden");
+    document.querySelector(".theme-switcher-button").addEventListener("click", function () {
+        document.querySelector(".light-theme-icon").classList.toggle("hidden");
+        document.querySelector(":root").classList.toggle("light");
+
+        document.querySelector(".dark-theme-icon").classList.toggle("hidden");
+        document.querySelector(":root").classList.toggle("dark");
+    });
+
     //
-    //     //
-    //     document.querySelector(".ololo").classList.toggle("hidden");
-    // });
 
     document.querySelector(".nav-switcher-button").addEventListener("click", function () {
         document.querySelector(".baseof-layout").classList.toggle("baseof-nav-hidden");
@@ -32,32 +33,15 @@ function init() {
 
     //
 
-    // document.querySelector(".language-switcher").addEventListener("click", (e) => {
-    //     document.querySelector(".language-switcher-list").classList.toggle("hidden");
-    //     document.querySelector(".ololo").classList.toggle("hidden");
-    // })
-
     document.querySelector(":root").addEventListener("click", (e) => {
         let var1 = e.target.closest(".language-switcher-button");
-        // let var2 = document.querySelector(".language-switcher-button");
-
-        // console.log(e.target)
-        // console.log(var1)
-        // console.log(e.target.matches(".language-switcher-button"));
 
         if (var1 === null) {
             document.querySelector(".language-switcher-list").classList.add("hidden");
         } else if (var1.matches(".language-switcher-button")) {
             document.querySelector(".language-switcher-list").classList.toggle("hidden");
         }
-
-        // if (var1 === var2) {
-        //     document.querySelector(".language-switcher-list").classList.toggle("hidden");
-        // } else {
-        //     document.querySelector(".language-switcher-list").classList.add("hidden");
-        // }
     });
-
 }
 
 
